ecc8c88986b5faefeff34e6d039a914f7888c00e
Patel Kathan Ketanbhai <kathan.9737283387@gmail.com>
2025-06-15 14:53:54 +0530

    Setting up Routes, Responsive design for Header and make Login and register page

--- Youtube_clone/src/main.jsx
+++ Youtube_clone/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+  {
+    path: "/login",
+    Component: Login,
+  },
+  {
+    path: "/register",
+    Component: Register,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ Youtube_clone/src/pages/Login.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router";
+
+function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = () => {};
+  return (
+    <>
+      <div className="w-screen h-screen bg-gradient-to-r from-[#89f7fe] to-[#c2e9fb] flex justify-center items-center">
+        <div className="w-[90%] h-[45%] lg:h-[50%] md:w-[60%] md:px-10 lg:w-[40%] bg-slate-100 shadow shadow-gray-600 p-2.5 rounded flex flex-col gap-2.5">
+          <div className="md:my-2">
+            <h1 className="uppercase my-2.5 text-lg font-semibold text-center md:text-xl">
+              Login to your account
+            </h1>
+          </div>
+          <div className="md:my-4">
+            <form
+              className="flex flex-col gap-4 md:gap-6"
+              action=""
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <input
+                {...register("email", { required: true })}
+                className="w-full shadow shadow-slate-300 rounded-sm px-2 py-2"
+                type="text"
+                placeholder="Your Email"
+              />
+              <input
+                {...register("password", { required: true })}
+                className="w-full shadow shadow-slate-300 rounded-sm px-2 py-2"
+                type="text"
+                placeholder="Password"
+              />
+              <input
+                className="w-full mt-2 px-2 py-1 bg-gradient-to-r from-sky-300 to-teal-300 rounded uppercase text-white font-bold cursor-pointer"
+                type="submit"
+                value="Sign In"
+              />
+            </form>
+          </div>
+          <div className="text-sm self-center mt-2">
+            <span className="">Don't have an account?</span>
+            <Link to="/register" className="underline font-medium mx-1">
+              Register
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
